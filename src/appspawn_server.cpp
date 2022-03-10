dd4b0469a5b3e5dc79b8210a266602e0e47743cf
xlei1030 <xionglei6@huawei.com>
2022-03-10 17:02:31 +0800

    修改野指针
    Signed-off-by: xlei1030 <xionglei6@huawei.com>

--- src/appspawn_server.cpp
+++ src/appspawn_server.cpp
@@ -365,8 +365,8 @@ bool AppSpawnServer::ServerMain(char *longProcName, int64_t longProcNameLen)
             isChildDie_ = false;
             auto iter = appMap_.find(childPid_);
             if (iter != appMap_.end()) {
-                appMap_.erase(iter);
                 APPSPAWN_LOGI("delete pid=%d in appMap", iter->first);
+                appMap_.erase(iter);
             }
         }
         if (this->appQueue_.empty()) {
