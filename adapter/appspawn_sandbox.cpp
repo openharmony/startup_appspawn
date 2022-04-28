5097a8c45fe18e285d534da30112b64d3717622b
Mupceet <laiguizhong@huawei.com>
2022-04-28 21:36:03 +0800

    fix:回退sandbox
    Signed-off-by: Mupceet <laiguizhong@huawei.com>

--- adapter/appspawn_sandbox.cpp
+++ adapter/appspawn_sandbox.cpp
@@ -330,31 +330,13 @@ static int32_t DoSandboxRootFolderCreate(const std::string &sandboxPackagePath)
     return 0;
 }
 
-static void MatchSandbox(AppSpawnClientExt *appProperty)
-{
-    if (appProperty == nullptr) {
-        return;
-    }
-    if (strcmp("system_basic", appProperty->property.apl) == 0) {
-        EnterSandbox("priv-app");
-    } else if (strcmp("normal", appProperty->property.apl) == 0) {
-        EnterSandbox("app");
-    } else if (strcmp("system_core ", appProperty->property.apl) == 0) {
-        EnterSandbox("app");
-    } else {
-        APPSPAWN_LOGE("AppSpawnServer::Failed to match appspawn sandbox %s", appProperty->property.apl);
-        EnterSandbox("app");
-    }
-}
-
 int32_t SetAppSandboxProperty(struct AppSpawnContent_ *content, AppSpawnClient *client)
 {
     int rc = 0;
     APPSPAWN_CHECK(client != NULL, return -1, "Invalid appspwn client");
     AppSpawnClientExt *appProperty = (AppSpawnClientExt *)client;
-    MatchSandbox(appProperty);
-    // create /mnt/sandbox/<packagename> path�?later put it to rootfs module
-    std::string sandboxPackagePath = "/";
+    // create /mnt/sandbox/<packagename> path later put it to rootfs module
+    std::string sandboxPackagePath = "/mnt/sandbox/";
     sandboxPackagePath += appProperty->property.bundleName;
     mkdir(sandboxPackagePath.c_str(), FILE_MODE);
 
