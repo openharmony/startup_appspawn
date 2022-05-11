a6a7c09cddc3fcc6abb21f5e58284527e43855b0
laiguizhong <laiguizhong@huawei.com>
2022-05-11 14:29:45 +0800

    修改L1 Linux启动失败
    Signed-off-by: laiguizhong <laiguizhong@huawei.com>

--- lite/appspawn_service.c
+++ lite/appspawn_service.c
@@ -28,6 +28,7 @@
 #include "ohos_init.h"
 #include "samgr_lite.h"
 #include "service.h"
+#include "securec.h"
 
 static const int INVALID_PID = -1;
 static const int CLIENT_ID = 100;
@@ -118,8 +119,11 @@ AppSpawnContent *AppSpawnCreateContent(const char *socketName, char *longProcNam
     APPSPAWN_LOGI("AppSpawnCreateContent %s", socketName);
     AppSpawnContentLite *appSpawnContent = (AppSpawnContentLite *)malloc(sizeof(AppSpawnContentLite));
     APPSPAWN_CHECK(appSpawnContent != NULL, return NULL, "Failed to alloc memory for appspawn");
+    int ret = memset_s(appSpawnContent, sizeof(AppSpawnContentLite), 0, sizeof(AppSpawnContentLite));
+    APPSPAWN_CHECK(ret == 0, free(appSpawnContent);
+        return NULL, "Failed to memset conent");
     appSpawnContent->content.longProcName = NULL;
-    appSpawnContent->content.longProcNameLen = NULL;
+    appSpawnContent->content.longProcNameLen = 0;
     g_appSpawnContentLite = appSpawnContent;
     return appSpawnContent;
 }
@@ -139,26 +143,25 @@ static int Invoke(IServerProxy *iProxy, int funcId, void *origin, IpcIo *req, Ip
         IpcIoPushInt64(reply, INVALID_PID);
         return EC_BADPTR;
     }
-
-    AppSpawnClientLite *client = (AppSpawnClientLite *)malloc(sizeof(AppSpawnClientLite));
-    APPSPAWN_CHECK(client != NULL, return -1, "malloc AppSpawnClientLite Failed");
-    client->client.id = CLIENT_ID;
-    client->client.flags = 0;
-    if (GetMessageSt(&client->message, req) != EC_SUCCESS) {
+    APPSPAWN_LOGI("[appspawn] invoke.");
+    AppSpawnClientLite client = {};
+    client.client.id = CLIENT_ID;
+    client.client.flags = 0;
+    if (GetMessageSt(&client.message, req) != EC_SUCCESS) {
         APPSPAWN_LOGE("[appspawn] invoke, parse failed! reply %d.", INVALID_PID);
         IpcIoPushInt64(reply, INVALID_PID);
         return EC_FAILURE;
     }
 
-    APPSPAWN_LOGI("[appspawn] invoke, msg<%s,%s,%d,%d %d>", client->message.bundleName, client->message.identityID,
-        client->message.uID, client->message.gID, client->message.capsCnt);
+    APPSPAWN_LOGI("[appspawn] invoke, msg<%s,%s,%d,%d %d>", client.message.bundleName, client.message.identityID,
+        client.message.uID, client.message.gID, client.message.capsCnt);
 
     pid_t newPid = 0;
-    int ret = AppSpawnProcessMsg(g_appSpawnContentLite, &client->client, &newPid);
+    int ret = AppSpawnProcessMsg(&g_appSpawnContentLite->content, &client.client, &newPid);
     if (ret != 0) {
         newPid = -1;
     }
-    FreeMessageSt(&client->message);
+    FreeMessageSt(&client.message);
     IpcIoPushInt64(reply, newPid);
 
 #ifdef OHOS_DEBUG
