1e9a52c5c65bd9a349ac905cd03b4c81f23f1c7d
xiacong <xiacong4@huawei.com>
2022-07-26 11:21:27 +0800

    <feat> 新增特性：系统进程沙盒环境支持Seccomp-BPF
    <body>
    对appspwan和webspwan的setresuid setresgid的参数进行限制
    Signed-off-by: xiacong <xiacong4@huawei.com>

--- adapter/appspawn_adapter.cpp
+++ adapter/appspawn_adapter.cpp
@@ -22,6 +22,10 @@
 #include "hap_restorecon.h"
 #endif
 #include "token_setproc.h"
+#ifdef WITH_SECCOMP
+#include "seccomp_policy.h"
+#endif
+
 
 void SetAppAccessToken(struct AppSpawnContent_ *content, AppSpawnClient *client)
 {
@@ -43,6 +47,17 @@ void SetSelinuxCon(struct AppSpawnContent_ *content, AppSpawnClient *client)
             errno, appProperty->property.apl);
     } else {
         APPSPAWN_LOGI("AppSpawnServer::Success to hap domain set context, ret = %d", ret);
+    }
+#endif
+}
+
+void SetUidGidFilter(struct AppSpawnContent_ *content)
+{
+#ifdef WITH_SECCOMP
+    if (!SetSeccompPolicy(APPSPAWN)) {
+        APPSPAWN_LOGE("AppSpawnServer::Failed to set APPSPAWN seccomp filter");
+    } else {
+        APPSPAWN_LOGI("AppSpawnServer::Success to set APPSPAWN seccomp filter");
     }
 #endif
 }
