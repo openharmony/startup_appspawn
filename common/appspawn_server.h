2c755d35d4a5d6236b213d3f6f267095a54513ea
chenshixu1 <chenshixu1@huawei.com>
2024-03-02 16:26:26 +0800

    NEXT_hilog
    Signed-off-by: chenshixu1 <chenshixu1@huawei.com>

--- common/appspawn_server.h
+++ common/appspawn_server.h
@@ -99,25 +99,22 @@ pid_t AppSpawnFork(int (*childFunc)(void *arg), void *args);
 #define UNUSED(x) (void)(x)
 
 #ifndef OHOS_LITE
-#define APPSPAWN_LOG(logLevel, domain, tag, fmt, ...) \
-    HiLogPrint(LOG_CORE, (LogLevel)logLevel, domain, tag, \
-        "[%{public}s:%{public}d]" fmt,  (FILE_NAME), (__LINE__), ##__VA_ARGS__)
 
-#ifndef APPSPAWN_LABEL
-#define APPSPAWN_LABEL "APPSPAWN"
-#endif
-#define APPSPAWN_DOMAIN (BASE_DOMAIN + 0x11)
+#undef LOG_TAG
+#define LOG_TAG "APPSPAWN"
+#undef LOG_DOMAIN
+#define LOG_DOMAIN (BASE_DOMAIN + 0x11)
 
 #define APPSPAWN_LOGI(fmt, ...) \
-    APPSPAWN_LOG(LOG_INFO, APPSPAWN_DOMAIN, APPSPAWN_LABEL, fmt, ##__VA_ARGS__)
+    HILOG_INFO(LOG_CORE, fmt, ##__VA_ARGS__)
 #define APPSPAWN_LOGE(fmt, ...) \
-    APPSPAWN_LOG(LOG_ERROR, APPSPAWN_DOMAIN, APPSPAWN_LABEL, fmt, ##__VA_ARGS__)
+    HILOG_ERROR(LOG_CORE, fmt, ##__VA_ARGS__)
 #define APPSPAWN_LOGV(fmt, ...) \
-    APPSPAWN_LOG(LOG_DEBUG, APPSPAWN_DOMAIN, APPSPAWN_LABEL, fmt, ##__VA_ARGS__)
+    HILOG_DEBUG(LOG_CORE, fmt, ##__VA_ARGS__)
 #define APPSPAWN_LOGW(fmt, ...) \
-    APPSPAWN_LOG(LOG_WARN, APPSPAWN_DOMAIN, APPSPAWN_LABEL, fmt, ##__VA_ARGS__)
+    HILOG_WARN(LOG_CORE, fmt, ##__VA_ARGS__)
 #define APPSPAWN_LOGF(fmt, ...) \
-    APPSPAWN_LOG(LOG_FATAL, APPSPAWN_DOMAIN, APPSPAWN_LABEL, fmt, ##__VA_ARGS__)
+    HILOG_FATAL(LOG_CORE, fmt, ##__VA_ARGS__)
 
 #else
 
