a477710e3feb199fb9ad5d6679e021e188b76c82
zhongning5 <zhongning5@huawei.com>
2024-04-01 21:25:16 +0800

    appspawn module
    Signed-off-by: zhongning5 <zhongning5@huawei.com>

---
+++ modules/modulemgr/appspawn_modulemgr.c
@@ -0,0 +1,261 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "appspawn_modulemgr.h"
+
+#include "appspawn_hook.h"
+#include "appspawn_manager.h"
+#include "appspawn_utils.h"
+#include "hookmgr.h"
+#include "modulemgr.h"
+
+typedef struct {
+    const AppSpawnContent *content;
+    const AppSpawnedProcessInfo *appInfo;
+} AppSpawnAppArg;
+
+static struct {
+    MODULE_MGR *moduleMgr;
+    AppSpawnModuleType type;
+    const char *moduleName;
+} g_moduleMgr[MODULE_MAX] = {
+    {NULL, MODULE_DEFAULT, "appspawn"},
+    {NULL, MODULE_APPSPAWN, "appspawn/appspawn"},
+    {NULL, MODULE_NWEBSPAWN, "appspawn/nwebspawn"},
+    {NULL, MODULE_COMMON, "appspawn/common"},
+};
+static HOOK_MGR *g_appspawnHookMgr = NULL;
+
+int AppSpawnModuleMgrInstall(const char *moduleName)
+{
+    if (moduleName == NULL) {
+        return -1;
+    }
+    int type = MODULE_DEFAULT;
+    if (g_moduleMgr[type].moduleMgr == NULL) {
+        g_moduleMgr[type].moduleMgr = ModuleMgrCreate(g_moduleMgr[type].moduleName);
+    }
+    if (g_moduleMgr[type].moduleMgr == NULL) {
+        return -1;
+    }
+#ifndef APPSPAWN_TEST
+    return ModuleMgrInstall(g_moduleMgr[type].moduleMgr, moduleName, 0, NULL);
+#else
+    return 0;
+#endif
+}
+
+void AppSpawnModuleMgrUnInstall(int type)
+{
+    if (type >= MODULE_MAX) {
+        return;
+    }
+    if (g_moduleMgr[type].moduleMgr == NULL) {
+        return;
+    }
+    ModuleMgrDestroy(g_moduleMgr[type].moduleMgr);
+    g_moduleMgr[type].moduleMgr = NULL;
+}
+
+int AppSpawnLoadAutoRunModules(int type)
+{
+    if (type >= MODULE_MAX) {
+        return -1;
+    }
+    if (g_moduleMgr[type].moduleMgr != NULL) {
+        return 0;
+    }
+    APPSPAWN_LOGI("AppSpawnLoadAutoRunModules: %{public}d moduleName: %{public}s", type, g_moduleMgr[type].moduleName);
+#ifndef APPSPAWN_TEST
+    g_moduleMgr[type].moduleMgr = ModuleMgrScan(g_moduleMgr[type].moduleName);
+    return g_moduleMgr[type].moduleMgr == NULL ? -1 : 0;
+#else
+    return 0;
+#endif
+}
+
+HOOK_MGR *GetAppSpawnHookMgr(void)
+{
+    if (g_appspawnHookMgr != NULL) {
+        return g_appspawnHookMgr;
+    }
+    g_appspawnHookMgr = HookMgrCreate("appspawn");
+    return g_appspawnHookMgr;
+}
+
+void DeleteAppSpawnHookMgr(void)
+{
+    HookMgrDestroy(g_appspawnHookMgr);
+    g_appspawnHookMgr = NULL;
+}
+
+static int PreloadHookRun(const HOOK_INFO *hookInfo, void *executionContext)
+{
+    AppSpawnHookArg *arg = (AppSpawnHookArg *)executionContext;
+    PreloadHook realHook = (PreloadHook)hookInfo->hookCookie;
+    return realHook((void *)arg->content);
+}
+
+static void PreHookExec(const HOOK_INFO *hookInfo, void *executionContext)
+{
+    AppSpawnHookArg *arg = (AppSpawnHookArg *)executionContext;
+    AppSpawnMgr *spawnMgr = (AppSpawnMgr *)arg->content;
+    clock_gettime(CLOCK_MONOTONIC, &spawnMgr->perLoadStart);
+    APPSPAWN_LOGI("Hook stage: %{public}d prio: %{public}d start", hookInfo->stage, hookInfo->prio);
+}
+
+static void PostHookExec(const HOOK_INFO *hookInfo, void *executionContext, int executionRetVal)
+{
+    AppSpawnHookArg *arg = (AppSpawnHookArg *)executionContext;
+    AppSpawnMgr *spawnMgr = (AppSpawnMgr *)arg->content;
+    clock_gettime(CLOCK_MONOTONIC, &spawnMgr->perLoadEnd);
+    uint64_t diff = DiffTime(&spawnMgr->perLoadStart, &spawnMgr->perLoadEnd);
+    APPSPAWN_LOGI("Hook stage: %{public}d prio: %{public}d end time %{public}" PRId64 " ns result: %{public}d",
+        hookInfo->stage, hookInfo->prio, diff, executionRetVal);
+}
+
+int PreloadHookExecute(AppSpawnContent *content)
+{
+    AppSpawnHookArg arg;
+    arg.content = content;
+    arg.client = NULL;
+    HOOK_EXEC_OPTIONS options;
+    options.flags = TRAVERSE_STOP_WHEN_ERROR;
+    options.preHook = PreHookExec;
+    options.postHook = PostHookExec;
+    int ret = HookMgrExecute(GetAppSpawnHookMgr(), STAGE_SERVER_PRELOAD, (void *)(&arg), &options);
+    APPSPAWN_LOGI("Execute hook [%{public}d] result %{public}d", STAGE_SERVER_PRELOAD, ret);
+    return ret == ERR_NO_HOOK_STAGE ? 0 : ret;
+}
+
+int AddPreloadHook(int prio, PreloadHook hook)
+{
+    APPSPAWN_CHECK(hook != NULL, return APPSPAWN_ARG_INVALID, "Invalid hook");
+    HOOK_INFO info;
+    info.stage = STAGE_SERVER_PRELOAD;
+    info.prio = prio;
+    info.hook = PreloadHookRun;
+    info.hookCookie = (void *)hook;
+    APPSPAWN_LOGI("AddPreloadHook prio: %{public}d", prio);
+    return HookMgrAddEx(GetAppSpawnHookMgr(), &info);
+}
+
+static int AppSpawnHookRun(const HOOK_INFO *hookInfo, void *executionContext)
+{
+    AppSpawnForkArg *arg = (AppSpawnForkArg *)executionContext;
+    AppSpawnHook realHook = (AppSpawnHook)hookInfo->hookCookie;
+    return realHook((AppSpawnMgr *)arg->content, (AppSpawningCtx *)arg->client);
+}
+
+static void PreAppSpawnHookExec(const HOOK_INFO *hookInfo, void *executionContext)
+{
+    AppSpawnHookArg *arg = (AppSpawnHookArg *)executionContext;
+    clock_gettime(CLOCK_MONOTONIC, &arg->tmStart);
+    APPSPAWN_LOGI("Hook stage: %{public}d prio: %{public}d start", hookInfo->stage, hookInfo->prio);
+}
+
+static void PostAppSpawnHookExec(const HOOK_INFO *hookInfo, void *executionContext, int executionRetVal)
+{
+    AppSpawnHookArg *arg = (AppSpawnHookArg *)executionContext;
+    clock_gettime(CLOCK_MONOTONIC, &arg->tmEnd);
+    uint64_t diff = DiffTime(&arg->tmStart, &arg->tmEnd);
+    APPSPAWN_LOGI("Hook stage: %{public}d prio: %{public}d end time %{public}" PRId64 " ns result: %{public}d",
+        hookInfo->stage, hookInfo->prio, diff, executionRetVal);
+}
+
+int AppSpawnHookExecute(AppSpawnHookStage stage, uint32_t flags, AppSpawnContent *content, AppSpawnClient *client)
+{
+    APPSPAWN_LOGV("Execute hook [%{public}d] for app: %{public}s", stage, GetProcessName((AppSpawningCtx *)client));
+    AppSpawnHookArg forkArg;
+    forkArg.client = client;
+    forkArg.content = content;
+    HOOK_EXEC_OPTIONS options;
+    options.flags = flags;  // TRAVERSE_STOP_WHEN_ERROR : 0;
+    options.preHook = PreAppSpawnHookExec;
+    options.postHook = PostAppSpawnHookExec;
+    int ret = HookMgrExecute(GetAppSpawnHookMgr(), stage, (void *)(&forkArg), &options);
+    return ret == ERR_NO_HOOK_STAGE ? 0 : ret;
+}
+
+int AppSpawnExecuteClearEnvHook(AppSpawnContent *content, AppSpawnClient *client)
+{
+    return AppSpawnHookExecute(STAGE_CHILD_PRE_COLDBOOT, HOOK_STOP_WHEN_ERROR, content, client);
+}
+
+int AppSpawnExecuteSpawningHook(AppSpawnContent *content, AppSpawnClient *client)
+{
+    return AppSpawnHookExecute(STAGE_CHILD_EXECUTE, HOOK_STOP_WHEN_ERROR, content, client);
+}
+
+int AppSpawnExecutePostReplyHook(AppSpawnContent *content, AppSpawnClient *client)
+{
+    return AppSpawnHookExecute(STAGE_CHILD_POST_RELY, HOOK_STOP_WHEN_ERROR, content, client);
+}
+
+int AppSpawnExecutePreReplyHook(AppSpawnContent *content, AppSpawnClient *client)
+{
+    return AppSpawnHookExecute(STAGE_CHILD_PRE_RELY, HOOK_STOP_WHEN_ERROR, content, client);
+}
+
+void AppSpawnEnvClear(AppSpawnContent *content, AppSpawnClient *client)
+{
+    (void)AppSpawnHookExecute(STAGE_CHILD_PRE_RUN, 0, content, client);
+}
+
+int AddAppSpawnHook(AppSpawnHookStage stage, int prio, AppSpawnHook hook)
+{
+    APPSPAWN_CHECK(hook != NULL, return APPSPAWN_ARG_INVALID, "Invalid hook");
+    HOOK_INFO info;
+    info.stage = stage;
+    info.prio = prio;
+    info.hook = AppSpawnHookRun;
+    info.hookCookie = (void *)hook;
+    APPSPAWN_LOGI("AddAppSpawnHook stage: %{public}d prio: %{public}d", stage, prio);
+    return HookMgrAddEx(GetAppSpawnHookMgr(), &info);
+}
+
+int ProcessMgrHookExecute(AppSpawnHookStage stage, const AppSpawnContent *content,
+    const AppSpawnedProcessInfo *appInfo)
+{
+    AppSpawnAppArg arg;
+    arg.appInfo = appInfo;
+    arg.content = content;
+    int ret = HookMgrExecute(GetAppSpawnHookMgr(), stage, (void *)(&arg), NULL);
+    return ret == ERR_NO_HOOK_STAGE ? 0 : ret;
+}
+
+static int ProcessMgrHookRun(const HOOK_INFO *hookInfo, void *executionContext)
+{
+    AppSpawnAppArg *arg = (AppSpawnAppArg *)executionContext;
+    ProcessChangeHook realHook = (ProcessChangeHook)hookInfo->hookCookie;
+    return realHook((AppSpawnMgr *)arg->content, arg->appInfo);
+}
+
+int AddProcessMgrHook(AppSpawnHookStage stage, int prio, ProcessChangeHook hook)
+{
+    APPSPAWN_CHECK(hook != NULL, return APPSPAWN_ARG_INVALID, "Invalid hook");
+    HOOK_INFO info;
+    info.stage = stage;
+    info.prio = prio;
+    info.hook = ProcessMgrHookRun;
+    info.hookCookie = hook;
+    return HookMgrAddEx(GetAppSpawnHookMgr(), &info);
+}
+
+void RegChildLooper(struct TagAppSpawnContent *content, ChildLoop loop)
+{
+    APPSPAWN_CHECK(content != NULL && loop != NULL, return, "Invalid content for RegChildLooper");
+    content->runChildProcessor = loop;
+}
---
+++ modules/sandbox/appspawn_mount_template.c
@@ -0,0 +1,187 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <errno.h>
+#include <fcntl.h>
+#include <sched.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <sys/mount.h>
+#include <sys/stat.h>
+#include <sys/syscall.h>
+#include <sys/types.h>
+
+#include "appspawn_sandbox.h"
+#include "appspawn_utils.h"
+#include "securec.h"
+
+/*
+名称            fs-type      flags                          options                 说明
+default                      MS_BIND MS_REC                                         默认可读写
+rdonly                       MS_NODEV MS_RDONLY                                     只读挂载
+epfs            epfs         MS_NODEV                                                待讨论
+dac_override                 MS_NODEV MS_RDONLY             "support_overwrite=1"
+开启const.filemanager.full_mount.enable fuse            fuse         MS_NOSUID MS_NODEV MS_NOEXEC MS_NOATIME MS_LAZYTIME
+dlp_fuse        fuse         MS_NOSUID MS_NODEV MS_NOEXEC MS_NOATIME MS_LAZYTIME     为dlpmanager管理应用专用的挂载参数
+shared                       MS_BIND MS_REC                                           root
+namespace上是MS_SHARED方式挂载
+
+*/
+/**
+    "dac-override-sensitive": "true",  默认值：false。使能后直接使用默认值
+    "sandbox-flags-customized": [ "MS_NODEV", "MS_RDONLY" ],
+    "fs-type": "sharefs",
+    "options": "support_overwrite=1"
+*/
+#define FUSE_MOUNT_FLAGS (MS_NOSUID | MS_NODEV | MS_NOEXEC | MS_NOATIME | MS_LAZYTIME)
+#define FUSE_MOUNT_OPTIONS  \
+    "fd=%d, rootmode=40000,user_id=%d,group_id=%d,allow_other," \
+    "context=\"u:object_r:dlp_fuse_file:s0\", fscontext=u:object_r:dlp_fuse_file:s0"
+
+static const MountArgTemplate DEF_MOUNT_ARG_TMP[MOUNT_TMP_MAX] = {
+    {"default", MOUNT_TMP_DEFAULT, NULL, MS_BIND | MS_REC, NULL, MS_SLAVE},
+    {"rdonly", MOUNT_TMP_RDONLY, NULL, MS_NODEV | MS_RDONLY, NULL, MS_SLAVE},
+    {"epfs", MOUNT_TMP_EPFS, "epfs", MS_NODEV, NULL, MS_SLAVE},
+    {"dac_override", MOUNT_TMP_DAC_OVERRIDE, "sharefs", MS_NODEV | MS_RDONLY, "support_overwrite=1", MS_SLAVE},
+    {"fuse", MOUNT_TMP_FUSE, "fuse", FUSE_MOUNT_FLAGS, FUSE_MOUNT_OPTIONS, MS_SHARED},
+    {"dlp_fuse", MOUNT_TMP_DLP_FUSE, "fuse", FUSE_MOUNT_FLAGS, FUSE_MOUNT_OPTIONS, MS_SHARED},
+    {"shared", MOUNT_TMP_SHRED, NULL, MS_BIND | MS_REC, NULL, MS_SHARED},
+};
+
+static const SandboxFlagInfo MOUNT_FLAGS_MAP[] = {
+    {"rec", MS_REC}, {"MS_REC", MS_REC},
+    {"bind", MS_BIND}, {"MS_BIND", MS_BIND}, {"move", MS_MOVE}, {"MS_MOVE", MS_MOVE},
+    {"slave", MS_SLAVE}, {"MS_SLAVE", MS_SLAVE}, {"rdonly", MS_RDONLY}, {"MS_RDONLY", MS_RDONLY},
+    {"shared", MS_SHARED}, {"MS_SHARED", MS_SHARED}, {"unbindable", MS_UNBINDABLE},
+    {"MS_UNBINDABLE", MS_UNBINDABLE}, {"remount", MS_REMOUNT}, {"MS_REMOUNT", MS_REMOUNT},
+    {"nosuid", MS_NOSUID}, {"MS_NOSUID", MS_NOSUID}, {"nodev", MS_NODEV}, {"MS_NODEV", MS_NODEV},
+    {"noexec", MS_NOEXEC}, {"MS_NOEXEC", MS_NOEXEC}, {"noatime", MS_NOATIME}, {"MS_NOATIME", MS_NOATIME},
+    {"lazytime", MS_LAZYTIME}, {"MS_LAZYTIME", MS_LAZYTIME}
+};
+
+static const SandboxFlagInfo PATH_MODE_MAP[] = {
+    {"S_IRUSR", S_IRUSR}, {"S_IWUSR", S_IWUSR}, {"S_IXUSR", S_IXUSR},
+    {"S_IRGRP", S_IRGRP}, {"S_IWGRP", S_IWGRP}, {"S_IXGRP", S_IXGRP},
+    {"S_IROTH", S_IROTH}, {"S_IWOTH", S_IWOTH}, {"S_IXOTH", S_IXOTH},
+    {"S_IRWXU", S_IRWXU}, {"S_IRWXG", S_IRWXG}, {"S_IRWXO", S_IRWXO}
+};
+
+uint32_t GetMountCategory(const char *name)
+{
+    APPSPAWN_CHECK_ONLY_EXPER(name != NULL, return MOUNT_TMP_DEFAULT);
+    uint32_t count = ARRAY_LENGTH(DEF_MOUNT_ARG_TMP);
+    for (uint32_t i = 0; i < count; i++) {
+        if (strcmp(DEF_MOUNT_ARG_TMP[i].name, name) == 0) {
+            return DEF_MOUNT_ARG_TMP[i].category;
+        }
+    }
+    return MOUNT_TMP_DEFAULT;
+}
+
+const MountArgTemplate *GetMountArgTemplate(uint32_t category)
+{
+    uint32_t count = ARRAY_LENGTH(DEF_MOUNT_ARG_TMP);
+    if (category > count) {
+        return NULL;
+    }
+    for (uint32_t i = 0; i < count; i++) {
+        if (DEF_MOUNT_ARG_TMP[i].category == category) {
+            return &DEF_MOUNT_ARG_TMP[i];
+        }
+    }
+    return NULL;
+}
+
+const SandboxFlagInfo *GetSandboxFlagInfo(const char *key, const SandboxFlagInfo *flagsInfos, uint32_t count)
+{
+    APPSPAWN_CHECK_ONLY_EXPER(key != NULL, return NULL);
+    APPSPAWN_CHECK_ONLY_EXPER(flagsInfos != NULL, return NULL);
+    for (uint32_t i = 0; i < count; i++) {
+        if (strcmp(flagsInfos[i].name, key) == 0) {
+            return &flagsInfos[i];
+        }
+    }
+    return NULL;
+}
+
+int GetPathMode(const char *name)
+{
+    APPSPAWN_CHECK_ONLY_EXPER(name != NULL, return 0);
+    const SandboxFlagInfo *info = GetSandboxFlagInfo(name, PATH_MODE_MAP, ARRAY_LENGTH(PATH_MODE_MAP));
+    if (info != NULL) {
+        return info->flags;
+    }
+    return 0;
+}
+
+static void DumpSandboxFlags(char *buffer, uint32_t bufferSize, unsigned long flags,
+    const SandboxFlagInfo *flagsInfos, uint32_t count)
+{
+    bool first = true;
+    size_t currLen = 0;
+    int len = 0;
+    unsigned long tmp = flags;
+    for (uint32_t i = 0; i < count; i++) {
+        if ((flagsInfos[i].flags & tmp) == 0) {
+            continue;
+        }
+        tmp &= ~(flagsInfos[i].flags);
+        if (!first) {
+            len = sprintf_s(buffer + currLen, bufferSize - currLen - 1, " %s ", "|");
+            APPSPAWN_CHECK_ONLY_EXPER(len > 0, return);
+            currLen += len;
+        }
+        first = false;
+        len = sprintf_s(buffer + currLen, bufferSize - currLen, "%s", flagsInfos[i].name);
+        APPSPAWN_CHECK_ONLY_EXPER(len > 0, return);
+        currLen += len;
+    }
+}
+
+static void DumpMode(const char *info, mode_t mode)
+{
+    APPSPAWN_CHECK_ONLY_EXPER(info != NULL, return);
+    char buffer[64] = {0};  // 64 to show flags
+    DumpSandboxFlags(buffer, sizeof(buffer), mode, PATH_MODE_MAP, ARRAY_LENGTH(PATH_MODE_MAP));
+    APPSPAPWN_DUMP("%{public}s[0x%{public}x] %{public}s", info, (uint32_t)(mode), buffer);
+}
+
+static void DumpMountFlags(const char *info, unsigned long mountFlags)
+{
+    APPSPAWN_CHECK_ONLY_EXPER(info != NULL, return);
+    char buffer[128] = {0};  // 64 to show flags
+    DumpSandboxFlags(buffer, sizeof(buffer), mountFlags, MOUNT_FLAGS_MAP, ARRAY_LENGTH(MOUNT_FLAGS_MAP));
+    APPSPAPWN_DUMP("%{public}s[0x%{public}x] %{public}s", info, (uint32_t)(mountFlags), buffer);
+}
+
+void DumpMountPathMountNode(const PathMountNode *pathNode)
+{
+    const MountArgTemplate *tmp = GetMountArgTemplate(pathNode->category);
+    if (tmp == NULL) {
+        return;
+    }
+    APPSPAPWN_DUMP("        sandbox node category: %{public}u(%{public}s)", tmp->category, tmp->name);
+    DumpMountFlags("        sandbox node mountFlags: ", tmp->mountFlags);
+    APPSPAPWN_DUMP("        sandbox node mountSharedFlag: %{public}s",
+        tmp->mountSharedFlag == MS_SLAVE ? "MS_SLAVE" : "MS_SHARED");
+    APPSPAPWN_DUMP("        sandbox node options: %{public}s", tmp->options ? tmp->options : "null");
+    APPSPAPWN_DUMP("        sandbox node fsType: %{public}s", tmp->fsType ? tmp->fsType : "null");
+    DumpMode("        sandbox node destMode: ", pathNode->destMode);
+    APPSPAPWN_DUMP("        sandbox node mountSharedFlag: %{public}s",
+        pathNode->mountSharedFlag ? "MS_SHARED" : "MS_SLAVE");
+}
