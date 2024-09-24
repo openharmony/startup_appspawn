db5016c1c074b761f4c571c12e2ec17568e2458d
王达 <wangda20@huawei.com>
2024-09-24 16:28:07 +0800

    修改检视意见
    Signed-off-by: 王达 <wangda20@huawei.com>

--- service/devicedebug/kill/src/devicedebug_kill.c
+++ service/devicedebug/kill/src/devicedebug_kill.c
@@ -30,6 +30,14 @@ extern "C" {
 #define DEVICEDEBUG_KILL_CMD_SIGNAL_INDEX 2
 #define DEVICEDEBUG_KILL_CMD_PID_INDEX 3
 
+APPSPAWN_STATIC void DeviceDebugShowKillHelp(void)
+{
+    printf("\r\nusage: devicedebug kill [options] <pid>"
+        "\r\noptions list:"
+        "\r\n         -h, --help         list available commands"
+        "\r\n         kill -9 -12111     send a signal to a process\r\n");
+}
+
 APPSPAWN_STATIC int DeviceDebugKill(char *signal, char *pid)
 {
     AppSpawnClientHandle clientHandle;
@@ -80,14 +88,22 @@ int DeviceDebugCmdKill(int argc, char *argv[])
         return DEVICEDEBUG_ERRNO_NOT_IN_DEVELOPER_MODE;
     }
 
+    if (argc <= DEVICEDEBUG_NUM_2 || strcmp(argv[DEVICEDEBUG_NUM_2], "-h") == 0 ||
+        strcmp(argv[DEVICEDEBUG_NUM_2], "help") == 0) {
+        DeviceDebugShowKillHelp();
+        return 0;
+    }
+
     if (argc < DEVICEDEBUG_KILL_CMD_NUM) {
         DEVICEDEBUG_LOGE("devicedebug cmd operator num is %{public}d < %{public}d", argc, DEVICEDEBUG_KILL_CMD_NUM);
+        DeviceDebugShowKillHelp();
         return DEVICEDEBUG_ERRNO_OPERATOR_ARGV_MISS;
     }
 
     int signal = atoi(argv[DEVICEDEBUG_KILL_CMD_SIGNAL_INDEX] + 1);
     if (signal > SIGRTMAX || signal <= 0) {
         DEVICEDEBUG_LOGE("signal is %{public}d > %{public}d", signal, SIGRTMAX);
+        DeviceDebugShowKillHelp();
         return DEVICEDEBUG_ERRNO_PARAM_INVALID;
     }
 
