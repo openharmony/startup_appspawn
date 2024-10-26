9f43656e08c017753ea0b40a82a183ce2a25075b
陈其文 <chenqiwen6@huawei.com>
2024-10-26 19:56:36 +0800

    Nwebspawn接入hungtask监测
    Signed-off-by: 陈其文 <chenqiwen6@huawei.com>

--- standard/appspawn_kickdog.c
+++ standard/appspawn_kickdog.c
@@ -24,7 +24,7 @@ static int OpenAndWriteToProc(const char *procName, const char *writeStr, size_t
         return -1;
     }
 
-    int procFd = open(procName, O_RDWR | O_CLOEXEC);
+    int procFd = open(procName, O_WRONLY | O_CLOEXEC);
     if (procFd == -1) {
         APPSPAWN_LOGE("open %{public}s fail,errno:%{public}d", procName, errno);
         return procFd;
@@ -41,49 +41,54 @@ static int OpenAndWriteToProc(const char *procName, const char *writeStr, size_t
     return writeResult;
 }
 
-// Enable watchdog monitoring
-static int OpenAppSpawnWatchdogFile(AppSpawnContent *content)
+static const char *GetProcFile(bool isLinux)
 {
-    APPSPAWN_LOGI("OpenAppSpawnWatchdogFile");
-    int result = 0;
-    if (access(LINUX_APPSPAWN_WATCHDOG_FILE, F_OK) == 0) {
-        result = OpenAndWriteToProc(LINUX_APPSPAWN_WATCHDOG_FILE, LINUX_APPSPAWN_WATCHDOG_ON,
-            strlen(LINUX_APPSPAWN_WATCHDOG_ON));
-    } else {
-        result = OpenAndWriteToProc(HM_APPSPAWN_WATCHDOG_FILE, HM_APPSPAWN_WATCHDOG_ON,
-            strlen(HM_APPSPAWN_WATCHDOG_ON));
+    if (isLinux) {
+        return LINUX_APPSPAWN_WATCHDOG_FILE;
     }
-    content->wdgOpened = (result != -1);
-    APPSPAWN_LOGI("open finish,result:%{public}d", result);
-    return result;
+    return HM_APPSPAWN_WATCHDOG_FILE;
 }
 
-static void KickAppSpawnWatchdog(void)
+static const char *GetProcContent(bool isLinux, bool isOpen, int mode)
 {
-    APPSPAWN_LOGI("kick proc begin");
-    int result = 0;
-    if (access(LINUX_APPSPAWN_WATCHDOG_FILE, F_OK) == 0) {
-        result = OpenAndWriteToProc(LINUX_APPSPAWN_WATCHDOG_FILE, LINUX_APPSPAWN_WATCHDOG_KICK,
-            strlen(LINUX_APPSPAWN_WATCHDOG_KICK));
-    } else {
-        result = OpenAndWriteToProc(HM_APPSPAWN_WATCHDOG_FILE, HM_APPSPAWN_WATCHDOG_KICK,
-            strlen(HM_APPSPAWN_WATCHDOG_KICK));
+    if (isOpen) {
+        if (isLinux) {
+            return LINUX_APPSPAWN_WATCHDOG_ON;
+        }
+        return (mode == MODE_FOR_NWEB_SPAWN) ? HM_NWEBSPAWN_WATCHDOG_ON : HM_APPSPAWN_WATCHDOG_ON;
     }
-    APPSPAWN_LOGI("kick end,result:%{public}d", result);
+
+    if (isLinux) {
+        return LINUX_APPSPAWN_WATCHDOG_KICK;
+    }
+    return (mode == MODE_FOR_NWEB_SPAWN) ? HM_NWEBSPAWN_WATCHDOG_KICK : HM_APPSPAWN_WATCHDOG_KICK;
+}
+
+static void DealSpawnWatchdog(AppSpawnContent *content, bool isOpen)
+{
+    APPSPAWN_LOGI("%{public}s %{public}s watchdog begin",
+        (content->mode == MODE_FOR_NWEB_SPAWN) ? "Nwebspawn" : "Appspawn", isOpen ? "enable" : "kick");
+    int result = 0;
+    const char *procFile = GetProcFile(content->isLinux);
+    const char *procContent = GetProcContent(content->isLinux, isOpen, content->mode);
+    result = OpenAndWriteToProc(procFile, procContent, strlen(procContent));
+    if (isOpen) {
+        content->wdgOpened = (result != -1);
+    }
+    APPSPAWN_LOGI("%{public}s %{public}s watchdog end, result:%{public}d",
+        (content->mode == MODE_FOR_NWEB_SPAWN) ? "Nwebspawn" : "Appspawn", isOpen ? "enable" : "kick", result);
 }
 
 static void ProcessTimerHandle(const TimerHandle taskHandle, void *context)
 {
     AppSpawnContent *wdgContent = (AppSpawnContent *)context;
 
-    APPSPAWN_LOGI("kick appspawn dog start");
-
     if (!wdgContent->wdgOpened) {  //first time deal kernel file
-        OpenAppSpawnWatchdogFile(wdgContent);
+        DealSpawnWatchdog(wdgContent, true);
         return;
     }
 
-    KickAppSpawnWatchdog();
+    DealSpawnWatchdog(wdgContent, false);
 }
 
 static void CreateTimerLoopTask(AppSpawnContent *content)
@@ -97,8 +102,42 @@ static void CreateTimerLoopTask(AppSpawnContent *content)
     APPSPAWN_CHECK(ret == 0, return, "Failed to start timerLoopTask ret %{public}d", ret);
 }
 
-void AppSpawnKickDogStart(AppSpawnContent *content)
+static int CheckKernelType(bool *isLinux)
 {
-    CreateTimerLoopTask(content);
-    OpenAppSpawnWatchdogFile(content);
+    struct utsname uts;
+    if (uname(&uts) == -1) {
+        APPSPAWN_LOGE("Kernel type get failed,errno:%{public}d", errno);
+        return -1;
+    }
+
+    if (strcmp(uts.sysname, "Linux") == 0) {
+        APPSPAWN_LOGI("Kernel type is linux");
+        *isLinux = true;
+        return 0;
+    }
+
+    *isLinux = false;
+    return 0;
+}
+
+static int SpawnKickDogStart(AppSpawnMgr *mgrContent)
+{
+    APPSPAWN_CHECK(mgrContent != NULL, return 0, "content is null");
+    APPSPAWN_CHECK((mgrContent->content.mode == MODE_FOR_APP_SPAWN) ||
+        (mgrContent->content.mode == MODE_FOR_NWEB_SPAWN), return 0, "Mode %{public}u no need enable watchdog",
+        mgrContent->content.mode);
+
+    if (CheckKernelType(&mgrContent->content.isLinux) != 0) {
+        return 0;
+    }
+
+    DealSpawnWatchdog(&mgrContent->content, true);
+    CreateTimerLoopTask(&mgrContent->content);
+
+    return 0;
+}
+
+MODULE_CONSTRUCTOR(void)
+{
+    AddPreloadHook(HOOK_PRIO_COMMON, SpawnKickDogStart);
 }
