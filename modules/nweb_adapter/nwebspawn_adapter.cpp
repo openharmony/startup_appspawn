d968559435f80bf7b3986c9da4f907344b46055e
zhaowenpu <zhaowenpu1@huawei.com>
2024-11-01 03:29:49 +0000

    update modules/nweb_adapter/nwebspawn_adapter.cpp.
    Signed-off-by: zhaowenpu <zhaowenpu1@huawei.com>

--- modules/nweb_adapter/nwebspawn_adapter.cpp
+++ modules/nweb_adapter/nwebspawn_adapter.cpp
@@ -112,11 +112,9 @@ APPSPAWN_STATIC int RunChildProcessor(AppSpawnContent *content, AppSpawnClient *
     Dl_namespace dlns;
     dlns_init(&dlns, "nweb_ns");
     dlns_create(&dlns, libPath.c_str());
-
     Dl_namespace ndkns;
     dlns_get("ndkns", &ndkns);
-    dlns_inherit(&dlns, &ndkns, "allow_all_shared_libs");
-    
+    dlns_inherit(&dlns, &ndkns, "allow_all_shared_libs");    
     // preload libweb_engine
     webEngineHandle =
         dlopen_ns(&dlns, engineLibName.c_str(), RTLD_NOW | RTLD_GLOBAL);
