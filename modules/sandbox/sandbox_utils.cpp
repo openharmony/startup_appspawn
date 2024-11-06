26152ccfba3f2cd9c4e469316e28444fd0531981
wangfeng <wangfeng277@huawei.com>
2024-11-06 14:32:10 +0800

    modify system/app dir mount point
    Signed-off-by: wangfeng <wangfeng277@huawei.com>

--- modules/sandbox/sandbox_utils.cpp
+++ modules/sandbox/sandbox_utils.cpp
@@ -706,7 +706,8 @@ static uint32_t ConvertFlagStr(const std::string &flagStr)
 {
     const std::map<std::string, int> flagsMap = {{"0", 0}, {"START_FLAGS_BACKUP", 1},
                                                  {"DLP_MANAGER", 2},
-                                                 {"DEVELOPER_MODE", 17}};
+                                                 {"DEVELOPER_MODE", 17},
+                                                 {"PREINSTALLED_HAP", 29}};
 
     if (flagsMap.count(flagStr)) {
         return 1 << flagsMap.at(flagStr);
