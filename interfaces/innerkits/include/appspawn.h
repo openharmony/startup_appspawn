2ec2a6356c37541e77a25794da2fcd209a5f9211
yh <yanghao236@huawei.com>
2024-11-26 15:58:17 +0800

    modify debug
    Signed-off-by: yh <yanghao236@huawei.com>
    Change-Id: Ic6192b45d2b50191baa60ad23917082b909d6b74

--- interfaces/innerkits/include/appspawn.h
+++ interfaces/innerkits/include/appspawn.h
@@ -277,6 +277,7 @@ int AppSpawnClientAddPermission(AppSpawnClientHandle handle, AppSpawnReqMsgHandl
 #define MSG_EXT_NAME_PROCESS_TYPE "ProcessType"
 #define MSG_EXT_NAME_MAX_CHILD_PROCCESS_MAX "MaxChildProcess"
 #define MSG_EXT_NAME_APP_FD "AppFd"
+#define MSG_EXT_NAME_USERID "uid"
 #define MSG_EXT_NAME_JIT_PERMISSIONS "JITPermissions"
 
 int AppSpawnReqMsgAddExtInfo(AppSpawnReqMsgHandle reqHandle, const char *name, const uint8_t *value, uint32_t valueLen);
