950d1621a7131ce4d98bfe32f654115f746f3cdc
fan-jingle <fanjingle@h-partners.com>
2026-02-10 12:01:54 +0800

    优化测试用例
    Signed-off-by: fan-jingle <fanjingle@h-partners.com>

---
+++ test/unittest/app_spawn_standard_test/app_spawn_isolate_test/app_spawn_isolate_test.cpp
@@ -0,0 +1,90 @@
+/*
+ * Copyright (c) 2026 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <gtest/gtest.h>
+
+#include "appspawn.h"
+#include "appspawn_hook.h"
+#include "appspawn_manager.h"
+#include "isolate_test_helper.h"
+#include "lib_wrapper.h"
+#ifdef __cplusplus
+extern "C" {
+#endif
+ 	 
+int SetIsolateDir(const AppSpawningCtx *property);
+ 	 
+#ifdef __cplusplus
+}
+#endif
+
+using namespace testing;
+using namespace testing::ext;
+using namespace OHOS;
+
+namespace OHOS {
+static IsolateTestHelper g_testHelper;
+class AppSpawnIsolateTest : public testing::Test {
+public:
+    static void SetUpTestCase() {}
+    static void TearDownTestCase() {}
+    void SetUp()
+    {
+        const TestInfo *info = UnitTest::GetInstance()->current_test_info();
+        GTEST_LOG_(INFO) << info->test_suite_name() << "." << info->name() << " start";
+        APPSPAWN_LOGI("%{public}s.%{public}s start", info->test_suite_name(), info->name());
+    }
+    void TearDown()
+    {
+        const TestInfo *info = UnitTest::GetInstance()->current_test_info();
+        GTEST_LOG_(INFO) << info->test_suite_name() << "." << info->name() << " end";
+        APPSPAWN_LOGI("%{public}s.%{public}s end", info->test_suite_name(), info->name());
+    }
+};
+
+HWTEST_F(AppSpawnIsolateTest, App_Spawn_SetIsolateDir, TestSize.Level0)
+{
+    AppSpawnClientHandle clientHandle = nullptr;
+    AppSpawnReqMsgHandle reqHandle = 0;
+    AppSpawningCtx *property = nullptr;
+    int ret = -1;
+
+    IoctlFunc ioctlFunc = [](int fd, int req, va_list args) -> int {
+        return 0;
+    };
+
+    UpdateIoctlFunc(ioctlFunc);
+    do {
+        // create msg
+        ret = AppSpawnClientInit(APPSPAWN_SERVER_NAME, &clientHandle);
+        APPSPAWN_CHECK(ret == 0, break, "Failed to create reqMgr %{public}s", APPSPAWN_SERVER_NAME);
+        reqHandle = g_testHelper.IsolateTestCreateSendMsg(clientHandle, MSG_APP_SPAWN);
+        APPSPAWN_CHECK(reqHandle != INVALID_REQ_HANDLE, break,
+            "Failed to create req %{public}s", APPSPAWN_SERVER_NAME);
+        // set custom sandbox flag
+        ret = AppSpawnReqMsgSetAppFlag(reqHandle, APP_FLAGS_CUSTOM_SANDBOX);
+        APPSPAWN_CHECK_ONLY_EXPER(ret == 0, break);
+        property = g_testHelper.IsolateTestGetAppProperty(clientHandle, reqHandle);
+        APPSPAWN_CHECK_ONLY_EXPER(property != nullptr, break);
+        ret = SetIsolateDir(property);
+    } while (0);
+
+    DeleteAppSpawningCtx(property);
+    AppSpawnClientDestroy(clientHandle);
+    UpdateIoctlFunc(nullptr);
+    ASSERT_EQ(ret, 0);
+}
+
+}  // namespace OHOS
