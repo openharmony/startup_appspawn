e8444c2f08e7a951c60a7f1f5318f97bec8b8ef1
s30043564 <shenxiaoliang1@huawei.com>
2026-03-21 18:50:10 +0800

    增加云推全局开关dfmalloc
    Signed-off-by: s30043564 <shenxiaoliang1@huawei.com>

--- util/src/appspawndf_utils.cpp
+++ util/src/appspawndf_utils.cpp
@@ -14,11 +14,14 @@
  */
 #include "appspawndf_utils.h"
 #include <atomic>
+#include <fstream>
 #include <mutex>
 #include <unordered_map>
 #include <unordered_set>
+#include <sstream>
 #include <string>
 #include <sys/random.h>
+#include <sys/stat.h>
 #include "appspawn_utils.h"
 #include "config_policy_utils.h"
 #include "parameters.h"
@@ -30,9 +33,15 @@ namespace AppSpawn {
 constexpr const char* APPSPAWNDF_LIST_PATH = "etc/memmgr/appspawndf_process.json";
 constexpr const char* APPSPAWN_DF_APPS = "appspawndf_apps";
 constexpr const char* GWP_ASAN_ENABLE = "gwp_asan.enable.app.";
+const std::string LOCAL_PATH = "/system/etc/SwitchOffList/";
+const std::string CLOUD_PATH = "/data/service/el1/public/update/param_service/install/system/etc/SwitchOffList/";
+const std::string APPSPAWN_DF_SWITCH_OFF_FLAG = "appspawn_df_switch_off_flag_v_0";
+constexpr int32_t VERSION_LEN = 4; // the number of digits extracted from the fixed format string "x.x.x.x"
 constexpr int32_t GWP_ASAN_NAME_LEN = 256;
 static std::atomic<uint8_t> g_processSampleRate = 128;
 static std::atomic<uint8_t> g_previousRandomValue = 0;
+constexpr int32_t MAX_VERSION_FILE_LEN = 1024 * 5;
+constexpr int32_t MAX_SWITCH_OFF_FILE_LEN = 1024 * 5;
 class AppSpawnDfUtils {
 public:
     static AppSpawnDfUtils &GetInstance()
@@ -122,8 +131,136 @@ public:
         return 0;
     }
 private:
+    static inline void Trim(std::string& inputStr)
+    {
+        if (inputStr.empty()) {
+            return;
+        }
+        inputStr.erase(inputStr.begin(), std::find_if(inputStr.begin(), inputStr.end(),
+            [](unsigned char ch) { return !std::isspace(static_cast<unsigned char>(ch)); }));
+        inputStr.erase(std::find_if(inputStr.rbegin(), inputStr.rend(),
+            [](unsigned char ch) { return !std::isspace(static_cast<unsigned char>(ch)); }).base(), inputStr.end());
+    }
+
+    static inline bool IsNumber(const std::string &str)
+    {
+        return !str.empty() && std::all_of(str.begin(), str.end(), [](unsigned char c) {
+            return std::isdigit(c);
+        });
+    }
+
+    std::vector<std::string> SplitString(const std::string& str, char pattern)
+    {
+        std::stringstream iss(str);
+        std::vector<std::string> result;
+        std::string token;
+        while (getline(iss, token, pattern)) {
+            result.emplace_back(token);
+        }
+        return result;
+    }
+
+    std::vector<std::string> GetVersionNums(const std::string& filePath)
+    {
+        struct stat fileStat;
+        if (stat(filePath.c_str(), &fileStat) != 0 ||
+            fileStat.st_size <= 0 || fileStat.st_size > MAX_VERSION_FILE_LEN) {
+            APPSPAWN_LOGE("AppSpawnDfUtils::GetVersionNums: stat failed");
+            return {};
+        }
+
+        std::ifstream file(filePath);
+        if (!file.is_open()) {
+            return {};
+        }
+        std::string line;
+        std::getline(file, line);
+        file.close();
+        if (line.empty()) {
+            APPSPAWN_LOGE("AppSpawnDfUtils::GetVersionNums line is empty!");
+            return {};
+        }
+        std::vector<std::string> versionStr = SplitString(line, '=');
+        const size_t expectedSize = 2; // 2: expect to divide the string into two parts base on "="
+        if (versionStr.size() != expectedSize) {
+            APPSPAWN_LOGE("AppSpawnDfUtils::GetVersionNums: version num is not valid, expected format 'key=value'.");
+            return {};
+        }
+        if (versionStr[1].empty()) { // index 1 indicates string representing the version number.
+            APPSPAWN_LOGE("AppSpawnDfUtils::GetVersionNums: version value is empty.");
+            return {};
+        }
+        Trim(versionStr[1]);
+        std::vector<std::string> versionNum = SplitString(versionStr[1], '.');
+        return versionNum;
+    }
+
+    bool CompareVersion(const std::vector<std::string> &localVersion, const std::vector<std::string> &cloudVersion)
+    {
+        if (localVersion.size() != VERSION_LEN || cloudVersion.size() != VERSION_LEN) {
+            APPSPAWN_LOGI("AppSpawnDfUtils::CompareVersion: version num is not valid, use localVersion.");
+            return false;
+        }
+        for (int32_t i = 0; i < VERSION_LEN; i++) {
+            if (localVersion[i] != cloudVersion[i]) {
+                if (!IsNumber(localVersion[i]) || !IsNumber(cloudVersion[i])) {
+                    APPSPAWN_LOGE("AppSpawnDfUtils::CompareVersion: version part contains non-numeric characters.");
+                    return false;
+                }
+                int32_t localValue = std::atoi(localVersion[i].c_str());
+                int32_t cloudValue = std::atoi(cloudVersion[i].c_str());
+                return localValue < cloudValue;
+            }
+        }
+        return false;
+    }
+
+    // Get path which contains a higher version configration file
+    std::string GetHigherVersionPath()
+    {
+        std::string localVersionFile = LOCAL_PATH + "version.txt"; // local version path
+        std::string cloudVersionFile = CLOUD_PATH + "version.txt"; // cloud version path
+        std::vector<std::string> localVersionNums = GetVersionNums(localVersionFile);
+        std::vector<std::string> cloudVersionNums = GetVersionNums(cloudVersionFile);
+        if (CompareVersion(localVersionNums, cloudVersionNums)) {
+            return CLOUD_PATH;
+        }
+        return LOCAL_PATH;
+    }
+
+    bool IsDisableDfmalloc()
+    {
+        std::string filePath = GetHigherVersionPath() + "switch_off_list";
+        struct stat fileStat;
+        if (stat(filePath.c_str(), &fileStat) != 0 ||
+            fileStat.st_size <= 0 || fileStat.st_size > MAX_SWITCH_OFF_FILE_LEN) {
+            APPSPAWN_LOGE("AppSpawnDfUtils::IsDisableDfmalloc: stat failed");
+            return false;
+        }
+        std::ifstream inputFile(filePath);
+        if (!inputFile.is_open()) {
+            APPSPAWN_LOGI("AppSpawnDfUtils::IsDisableDfmalloc: Can not open the file.");
+            return false;
+        }
+        std::string line;
+        while (std::getline(inputFile, line)) {
+            Trim(line);
+            if (line == APPSPAWN_DF_SWITCH_OFF_FLAG) {
+                APPSPAWN_LOGI("AppSpawnDfUtils::IsDisableDfmalloc: close dfmalloc");
+                inputFile.close();
+                return true;
+            }
+        }
+        inputFile.close();
+        return false;
+    }
+
     void LoadWhiteList()
     {
+        if (IsDisableDfmalloc()) {
+            return;
+        }
+
         char buf[PATH_MAX] = {0};
         /* 修正：使用 APPSPAWNDF_LIST_PATH */
         char *path = GetOneCfgFile(APPSPAWNDF_LIST_PATH, buf, PATH_MAX);
