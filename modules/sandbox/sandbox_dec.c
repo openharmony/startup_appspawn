6e34c622e0f89f39e409d5c6f3a56c4be83ab59e
gcw_lFHA1ZW9 <hupeng59@h-partners.com>
2026-05-28 22:54:46 +0800

    update: 更新文件 sandbox_dec.c
    Signed-off-by: gcw_lFHA1ZW9 <hupeng59@h-partners.com>

--- modules/sandbox/sandbox_dec.c
+++ modules/sandbox/sandbox_dec.c
@@ -47,9 +47,9 @@ static const DecIgnoreCaseInfo g_setInfo[] = { DEC_IGNORE_CASE_LIST };
 APPSPAWN_STATIC int SetIgnoreCaseDirs(AppSpawnMgr *content)
 {
     if (!IsNoShareFsEnable()) {
-    APPSPAWN_LOGV("ignore case dec not enable");
-    return 0;
- 	  }
+        APPSPAWN_LOGV("ignore case dec not enable");
+        return 0;
+    }
     APPSPAWN_CHECK(IsNativeSpawnMode(content) || IsAppSpawnMode(content), return 0,
         "not support ignore case dir");
     const char *decFilename = "/dev/dec";
