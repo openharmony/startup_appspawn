cd5ad7ef218e7f46ab99454227955f59a77d8346
m0_52228614 <liupu18@h-partners.com>
2026-08-04 17:53:23 +0800

    commit7.0
    Signed-off-by: m0_52228614 <liupu18@h-partners.com>

--- modules/common/appspawn_dfx_dump.cpp
+++ modules/common/appspawn_dfx_dump.cpp
@@ -19,6 +19,7 @@
 
 #ifndef APPSPAWN_TEST
 #include "dfx_dump_catcher.h"
+#include "faultloggerd_client.h"
 #endif
 
 void DumpSpawnStack(pid_t pid)
@@ -32,5 +33,13 @@ void DumpSpawnStack(pid_t pid)
     } else {
         APPSPAWN_LOGE("dumpCatch stackTrace for pid %{public}d failed", pid);
     }
+#endif
+}
+void SaveCoredump(void)
+{
+#ifndef APPSPAWN_TEST
+    APPSPAWN_LOGI("SaveCoredumpstart");
+	
+    SaveCoredumpToFileTimeout(getpid(), (10 * 1000));
 #endif
 }
